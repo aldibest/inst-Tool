dd7e90e23d930c1b385501b774bf5a92ea07d967
Lafeer-007 <59358628+Lafeer-007@users.noreply.github.com>
2019-12-30 14:47:22 +0530

    Add files via upload

---
+++ inst.py
@@ -0,0 +1,90 @@
+import os
+import sys 
+import time
+os.system("clear")
+print """
+  \033[1;35m
+|----------------------------------------------
+|.  Author :: Muhamed Lafeer                  |                           
+|.                                            |
+|.  coded by::Tech Teams                      |
+|.                                            |
+|.  Whatsapp no::7200200801                   |
+|.                                            |
+|.  Follow me on:Instagram::ig_lafeer         |
+|.                                            |
+|.  Version::2.0.                             |
+|.                                            |
+|---------------------------------------------|\033[1;36m
+                                           """
+                                           
+                                             
+
+print """
+
+          \033[1;36m===============================================\033[1;m
+          \033[1;33m|.    Install Best Hacking Tool               |
+         \033[1;36m ===============================================\033[1;m
+                              TOOLS*
+          
+             1.Weeman  
+             2.metasploit framework
+             3.Hidden Eye
+             4.Sms-Bomber
+             5.termux-Banner
+             6.Red Hawk
+             7.Tool-x
+             8.DarkFly    
+             9.webSploit
+             10.exit
+             99.About
+             """
+def weeman():
+  os.system("git clone https://github.com/evait-security/weeman")
+  
+          
+a = input("Enter the number~}>")
+if a==1:
+  weeman()
+if a==2:
+   os.system("apt install unstable-repo")
+   os.system("pkg install metasploit")
+   os.system("python2 inst.py")
+if a==3:
+  os.system("git clone https://github.com/DarkSecDevelopers/HiddenEye")
+  os.system("clear")
+  os.system("python2 inst.py")
+if a==4:
+  os.system("git clone https://github.com/Bhai4You/SmS-BomB")
+  os.system("clear")
+  os.system("python2 inst.py")
+if a==5:
+  os.system("git clone https://github.com/Bhai4You/Termux-Banner")
+  os.system("clear")
+  os.system("python2 inst.py")
+if a==6:
+  os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK")
+  os.system("python2 inst.py")
+if a==7:
+  os.system("git clone https://github.com/Rajkumrdusad/Tool-X")
+  os.system("python2 inst.py")
+if a==8:
+  os.system("git clone https://github.com/Ranginang67/DarkFly-Tool")
+  os.system("python2 inst.py")
+if a==9:
+  os.system("git clone https://github.com/websploit/websploit")
+if a==10:
+  os.system("exit")
+if a==99:
+  print("Author : Muhamed Lafeer")
+  print("coded by : Tech Hacker")
+  print("WhatsApp : 7200200801")
+  
+else:
+  os.system("python2 b.py")
+  print("Entered wrong")
+  
+def about():
+  print("Author : Muhamed Lafeer")
+  print("coded by : Tech Hacker")
+  print("WhatsApp : 7200200801")
---
+++ install.sh
@@ -0,0 +1,8 @@
+pkg install figlet
+pkg install git
+pkg install python
+pkg install python2
+pkg install php
+pkg install tolet
+pkg install sl
+sl
