e0a0b2d0f4eecfcc047ee69a5c127d6fc3d4ff68
Muhamed Lafeer <59358628+Lafeer-007@users.noreply.github.com>
2020-01-05 14:40:36 +0530

    Update inst.py

--- inst.py
+++ inst.py
@@ -77,7 +77,7 @@ if a==10:
   os.system("exit")
 if a==99:
 
-  print""\033[35m
+  print""\033[1;35m
          -------------------------
          |Author : Muhamed Lafeer|
          |coded by : Tech Hacker |
